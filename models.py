d0cd08c56fbe8bb0a2ab112c9ef3153e986a5c56
konstantinlaskulov <konstantin.laschkulov@yandex.com>
2024-11-09 19:56:36 +0300

    new validation models

--- models.py
+++ models.py
@@ -6,10 +6,20 @@ class HomeWork(BaseModel):
     date: str
     count: int = Field(0, ge=0)
 
+class HWPatch(BaseModel):
+    token: str 
+    date: str
+    count: int = Field(0, ge=0)
+
+class TemplatePatch(BaseModel):
+    token: str
+    new_template: str
+
 class User(BaseModel):
     telegram_id: int = Field(...)
     token: str = Field(...)
     homeworks: Optional[List[HomeWork]] = Field(default=[])
+    template: Optional[str] = Field(default='Здравствуйте! Все хорошо, зачет!')
 
     class Config:
         json_schema_extra = {
@@ -17,6 +27,7 @@ class User(BaseModel):
             {
                 "telegram_id": 0000000,
                 "token": "2faff0d80f02553281a64c4cedb08b50",
-                "homeworks": [{"date": "2024-10-22", "count": 10}, {"date": "2024-10-21", "count": 30}]
+                "homeworks": [{"date": "2024-10-22", "count": 10}, {"date": "2024-10-21", "count": 30}],
+                "template": "Здравствуйте! Это тествое сообщение!"
             }
         }
